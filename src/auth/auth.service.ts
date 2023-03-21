a2e3b1131a74d279bf4c7c6b8720012da122d477
theakashyadav <theakashyadavlko@gmail.com>
2023-03-21 15:55:18 +0530

    nest js first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    try {
+      // generate the password
+      const hash = await argon.hash(dto.password);
+
+      // save the user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  signin() {
+    // signin
+  }
+}
